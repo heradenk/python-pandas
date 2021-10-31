e336b037a14eca5a7fd512871e69e6f4f16f1594
heradenk <hera21denk@gmail.com>
2021-10-31 20:21:28 +0900

    Practice

---
+++ 04_pandas_visualize/04_20_matplotlib_scatter.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 31 19:51:40 2021
+
+@author: YOO
+"""
+
+# 라이브러리 불러오기
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.style.use('default')    # 스타일 서식 지정
+
+# read_csv() 함수로 df 생성
+df = pd.read_csv('./auto-mpg.csv', header=None)
+
+# 열 이름 지정
+df.columns = ['mpg', 'cylindedrs', 'displacement', 'horsepower', 'weight',
+              'acceleration', 'model year', 'origin', 'name']
+
+# 연비(mpg)와 차중(weight) 열에 대한 산점도 그리기
+df.plot(kind='scatter', x='weight', y='mpg', c='coral', s=10, figsize=(10, 5))
+plt.title('Scatter Plot - mpg vs weight')
+plt.show()
